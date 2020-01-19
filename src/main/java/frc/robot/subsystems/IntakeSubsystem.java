f2ea94b425fb9aed99612d0f64db6dc9f37f5430
Team5901 <35078583+Team5901@users.noreply.github.com>
2020-01-19 10:54:26 -0500

    bug fix

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,6 +9,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
@@ -19,7 +20,7 @@ public class IntakeSubsystem extends SubsystemBase {
    * Creates a new IntakeSubsystem.
    */
   private final WPI_VictorSPX boss = new WPI_VictorSPX(IntakeConstants.kMotorPort);
-  private final Compressor joe = new Compressor();
+  private final Compressor joe = new Compressor(0);
   private final Solenoid phil = new Solenoid(7);
   
   public IntakeSubsystem() {
@@ -33,14 +34,13 @@ public class IntakeSubsystem extends SubsystemBase {
 
   
   public void compressorOn(){
-       //joe.setClosedLoopControl(true);
+       joe.setClosedLoopControl(true);
   }
 
   public void compressorOff(){
-      //joe.setClosedLoopControl(false);
+      joe.setClosedLoopControl(false);
 
   }
-
 
   public void solenoidOn(){
     phil.set(true);
